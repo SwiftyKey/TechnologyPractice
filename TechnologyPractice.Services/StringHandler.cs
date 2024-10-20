c86fc5c6b43c8d53effa9d0cd8eb18df6acdcc62
SwiftyKey <slipperyfish9@gmail.com>
2024-10-20 18:00:45 +0500

    Выполнил первое задание

---
+++ TechnologyPractice.Services/StringHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace TechnologyPractice.Services;
+
+public static class StringHandler
+{
+	public static bool IsEven(int length) => length % 2 == 0;
+
+	public static string ReverseEvenString(string str)
+	{
+		string firstPart = str[..(str.Length / 2)];
+		string secondPart = str[(str.Length / 2)..];
+
+		return new string(firstPart.Reverse().Concat(secondPart.Reverse()).ToArray());
+	}
+
+	public static string ReverseOddString(string str)
+	{
+		return new string(str.Reverse().ToArray()) + str;
+	}
+
+	public static string Reverse(string source)
+	{
+		string result;
+
+		if (IsEven(source.Length)) result = ReverseEvenString(source);
+		else result = ReverseOddString(source);
+
+		return result;
+	}
+}
---
+++ TechnologyPractice/Controllers/StringHandlerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechnologyPractice.Services;
+
+namespace TechnologyPractice.Controllers;
+
+[Route("api/controller")]
+[ApiController]
+public class StringHandlerController : Controller
+{
+	[HttpGet()]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	public ActionResult<string> Get(string input)
+	{
+		return Ok(StringHandler.Reverse(input));
+	}
+}
