8b93edb1bfd0309eaf22d7c419ae07e95dffccd1
SwiftyKey <slipperyfish9@gmail.com>
2024-10-26 23:35:17 +0500

    Выполнил второе задание

---
+++ TechnologyPractice/Models/InputString.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnologyPractice.Models;
+
+public class InputString
+{
+	[Required]
+	[LowercaseEnglishLetters]
+	public string Text { get; set; }
+}
