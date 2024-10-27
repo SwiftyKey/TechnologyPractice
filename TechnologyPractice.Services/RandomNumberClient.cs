3f6c06cf79d4473dfdfb4fa6bfa2151304e20412
SwiftyKey <slipperyfish9@gmail.com>
2024-10-27 15:41:46 +0500

    Выполнил Задание 6

---
+++ TechnologyPractice.Services/RandomNumberClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TechnologyPractice.Services;
+
+public static class RandomNumberClient
+{
+	private static readonly Regex _rg = new(@"\d+");
+	public static string Url { get; set; } = "http://www.randomnumberapi.com/api/v1.0/random?max=";
+
+	public static async Task<int> GetValue(int maxValue, HttpClient client)
+	{
+		var response = await client.GetAsync(Url + maxValue);
+		int value;
+
+		if (response.IsSuccessStatusCode)
+        {
+			string result = await response.Content.ReadAsStringAsync();
+
+			value = int.Parse(_rg.Match(result).Value);
+		}
+		else
+			value = new Random().Next(maxValue);
+
+		return value;
+    }
+}
--- TechnologyPractice/Models/ResponseModel.cs
+++ TechnologyPractice/Models/ResponseModel.cs
@@ -6,4 +6,5 @@ public class ResponseModel
 	public IDictionary<char, int> Counter { get; set; }
 	public string LargestSubstring { get; set; }
 	public string SortedString { get; set; }
+	public string TruncatedString { get; set; }
 }
